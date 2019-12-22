5ec4cd3f5751dde2cb5bdcc3f20259a5a33b51bc
Brett Viren <brett.viren@gmail.com>
2019-12-22 12:11:25 -0500

    Test multi-client/server back and forth.

--- test/test_cs.cpp
+++ test/test_cs.cpp
@@ -1,41 +1,98 @@
 #include <czmq.h>
 
+#include <map>
+#include <vector>
 
 void test_owoa(bool backwards)
 {
-    zsock_t* c = zsock_new(ZMQ_CLIENT);
-    assert(c);
+    const int nclients = 3;
+    std::vector<zsock_t*> clients;
+    for (int ind=0; ind<nclients; ++ind) {
+        zsock_t* c = zsock_new(ZMQ_CLIENT);
+        assert(c);
+        clients.push_back(c);
+    }
     zsock_t* s = zsock_new(ZMQ_SERVER);
     assert(s);
 
-    zsock_t* tobind=s, *toconn=c;
-    if (backwards) {
-        tobind=c;
-        toconn=s;
-    }
-    //const char * addr = "tcp://127.0.0.1:5678";
-    const char * addr = "ipc://test_cs.ipc";
-    int rc = 0;
-    rc = zsock_bind(tobind, "%s", addr);
-    if (rc < 0 ) {
-        zsys_error("%d %s", errno, strerror(errno));
-    }
-    assert (rc >= 0);
-    rc = zsock_connect(toconn, "%s", addr);
-    assert(rc >= 0);
+    const char* mode[] = {"forward","backward"};
+    zsys_debug("made server and %d clients in %s mode",
+               nclients, mode[backwards]);
 
-    int n = 42;
-    rc = zsock_bsend(c,"4",n);
-    assert(rc >= 0);
-    n=0;
-    rc = zsock_brecv(s,"4",&n);
-    uint32_t rid = zsock_routing_id(s);
-    zsys_debug("%d %d %u bw:%d", rc, n, rid, backwards);
-    assert(rc == 0);
-    assert(n==42);
-    assert(rid != 0);
+    const int base_port = 5670;
+    //const char * addr = "tcp://127.0.0.1:%d";
+    const char * addr = "ipc://test_cs%d.ipc";
+    if (backwards) {            // bind clients, connect server
+        for (int ind=0; ind<nclients; ++ind) {
+            auto c = clients[ind];
+            const int port = base_port + ind;
+            const int p = zsock_bind(c, addr, port);
+            assert (p >= 0);
+            int rc = zsock_connect(s, addr, port);
+            assert (rc >= 0);
+        }
+    }
+    else {                      // bind server, connect clients
+        const int port = base_port;
+        const int p = zsock_bind(s, addr, port);
+        assert(p >= 0);
+        for (auto c : clients) {
+            int rc = zsock_connect(c, addr, port);
+            assert (rc >= 0);
+        }
+    }
+        
+    zsys_debug("bind and connect done");
+    //zclock_sleep(1000);
 
-    zsock_destroy(&c);
+    const int basen = 42;
+    // client send
+    for (int ind=0; ind<nclients; ++ind) {
+        auto c = clients[ind];
+        const int n = basen + ind;
+        zsys_debug("client sending %d", n);
+        const int rc = zsock_bsend(c,"4",n);
+        assert(rc >= 0);
+    }
+
+    zsys_debug("clients sent");
+
+    std::map<uint32_t, int> rid2n;
+
+    // server recv
+    for (int ind=0; ind<nclients; ++ind) {
+        int n = 0;
+        const int rc = zsock_brecv(s,"4",&n);
+        uint32_t rid = zsock_routing_id(s);
+        zsys_debug("server recving %d from 0x%x", n, rid);
+        rid2n[rid] = n;
+    }
+    
+    zsys_debug("server received");
+
+    // server send
+    for (auto& rn : rid2n) {
+        zsock_set_routing_id(s, rn.first);
+        zsys_debug("server sending %d to 0x%x", rn.second, rn.first);
+        int rc = zsock_bsend(s,"4",rn.second);
+        assert (rc >= 0);
+    }
+
+    zsys_debug("server sent");
+
+    // client recv
+    for (int ind=0; ind<nclients; ++ind) {
+        int n=0;
+        int rc = zsock_brecv(clients[ind],"4",&n);
+        zsys_debug("client recving %d", n);
+        assert(n == basen + ind);
+    }
+
+    zsys_debug ("clients received");
+
+    for (auto c : clients) {
+        zsock_destroy(&c);
+    }
     zsock_destroy(&s);
 }
 
