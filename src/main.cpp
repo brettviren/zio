6518c07e44b0fdd4946078a6e05dce81b54c5505
Brett Viren <brett.viren@gmail.com>
2020-03-20 14:42:44 -0400

    Fix #3. Downgrade from spdlog master to release 1.5.0
    This keeps limited support for env. var. SPDLOG_LEVEL=debug

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,10 @@
 #include "zio/util.hpp"
 #include "zio/logging.hpp"
 
-#include <spdlog/cfg/env.h>
+// this comes post 1.5.0
+// #include <spdlog/cfg/env.h>
+#include <cstdlib>
+#include <spdlog/logger.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
 void zio::init_signals()
@@ -10,9 +13,22 @@ void zio::init_signals()
     zio::catch_signals();
 }
 
+// until load_env release
+static
+void load_env_levels()
+{
+    auto env_val = std::getenv("SPDLOG_LEVEL"); // only support a simple level name
+    if (!env_val) {
+        return;
+    }
+    spdlog::set_level(spdlog::level::from_str(env_val));
+}
+
 void zio::init_logging()
 {
-    spdlog::cfg::load_env_levels();
+
+    //spdlog::cfg::load_env_levels();
+    load_env_levels();
     auto console = spdlog::stdout_color_mt("console");    
     auto err_logger = spdlog::stderr_color_mt("stderr");    
     spdlog::set_default_logger(console);
