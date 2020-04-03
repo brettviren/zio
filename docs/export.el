f148885dbcebf61c003838023e9282c76afe2002
Brett Viren <brett.viren@gmail.com>
2020-04-03 05:49:46 -0400

    Fix automatic running of dot from emacs and some make dependencies

--- docs/export.el
+++ docs/export.el
@@ -1,2 +1,17 @@
-
+(load-file "htmlize.el") 
 (require 'org)
+;;(require 'ob-python)
+(setq org-confirm-babel-evaluate nil)  ;trusting!
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '(
+;;   (python . t)
+;;   (ipython . t)
+   (shell . t)
+   (ditaa . t)
+   (dot . t)
+   (C . t)
+   (haskell . t)
+   (sqlite . t)
+   (plantuml . t)
+   ))
