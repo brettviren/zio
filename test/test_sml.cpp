b9e40790bc43f4942240a96ae4bc5857a01ab7c6
Brett Viren <brett.viren@gmail.com>
2020-04-29 15:29:02 -0400

    And states

--- test/test_sml.cpp
+++ test/test_sml.cpp
@@ -44,6 +44,11 @@ constexpr auto is_valid = [](const auto& event) { return event.valid; };
 constexpr auto send_fin = [](sender& s) { s.send(foo::fin{0}); };
 constexpr auto send_ack = [](const auto& event, sender& s) { s.send(event); };
 
+    struct established {};
+    struct fin_wait_1 {};
+    struct fin_wait_2 {};
+    struct timed_wait {};
+
 } // namespace foo
 
 // State Machine
@@ -59,10 +64,10 @@ struct tcp_release /*final*/
          */
         // clang-format off
         return make_transition_table(
-            *"established"_s + event<foo::release> / foo::send_fin = "fin wait 1"_s,
-            "fin wait 1"_s + event<foo::ack>[foo::is_valid] = "fin wait 2"_s,
-            "fin wait 2"_s + event<foo::fin>[foo::is_valid] / foo::send_ack = "timed wait"_s,
-            "timed wait"_s + event<foo::timeout> = X);
+            *state<foo::established> + event<foo::release> / foo::send_fin = state<foo::fin_wait_1>,
+            state<foo::fin_wait_1> + event<foo::ack>[foo::is_valid] = state<foo::fin_wait_2>,
+            state<foo::fin_wait_2> + event<foo::fin>[foo::is_valid] / foo::send_ack = state<foo::timed_wait>,
+            state<foo::timed_wait> + event<foo::timeout> = X);
         // clang-format on
     }
 };
@@ -75,16 +80,16 @@ int main()
 
     sender s{};
     sm<tcp_release> mysm{s};  // pass dependencies via ctor
-    assert(mysm.is("established"_s));
+    assert(mysm.is(state<foo::established>));
 
     mysm.process_event(foo::release{});  // complexity O(1)
-    assert(mysm.is("fin wait 1"_s));
+    assert(mysm.is(state<foo::fin_wait_1>));
 
     mysm.process_event(foo::ack{true});  // prints 'send: 0'
-    assert(mysm.is("fin wait 2"_s));
+    assert(mysm.is(state<foo::fin_wait_2>));
 
     mysm.process_event(foo::fin{42, true});  // prints 'send: 42'
-    assert(mysm.is("timed wait"_s));
+    assert(mysm.is(state<foo::timed_wait>));
 
     mysm.process_event(foo::timeout{});
     assert(mysm.is(X));  // terminated
